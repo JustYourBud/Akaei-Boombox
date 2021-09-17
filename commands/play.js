1ebbb0a92ca54f92943c5303bac3be13a82afdbf
JustYourBud <baconhatbuddy@gmail.com>
2021-09-17 10:29:44 -0500

    Add error template.

--- commands/play.js
+++ commands/play.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { Player } = require("discord-player");
+const { errorSend } = require("../templates/error.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,26 +11,23 @@ module.exports = {
         .setDescription("A search input or a link")
         .setRequired(true)
     ),
-  async execute(interaction, client) {
-    const player = new Player(client);
-    player.on("trackStart", (queue, track) =>
-      queue.metadata.channel.send(`🎶💜 | Now playing **${track.title}**!`)
-    );
-
+  async execute(interaction, player) {
     if (!interaction.member.voice.channelId)
-      return await interaction.reply({
-        content: "You are not in a voice channel, silly!",
-        ephemeral: true,
-      });
+      return errorSend(
+        interaction,
+        "Can you even hear me?",
+        "You need to be in a voice channel to use this command, silly!"
+      );
     if (
       interaction.guild.me.voice.channelId &&
       interaction.member.voice.channelId !==
         interaction.guild.me.voice.channelId
     )
-      return await interaction.reply({
-        content: "We need to be in the same voice channel!",
-        ephemeral: true,
-      });
+      return errorSend(
+        interaction,
+        "Don't be shy!",
+        "Looks like we'll need to be in the same voice channel for this to work."
+      );
     const query = interaction.options.get("query").value;
     const queue = player.createQueue(interaction.guild, {
       metadata: {
@@ -44,10 +41,11 @@ module.exports = {
         await queue.connect(interaction.member.voice.channel);
     } catch {
       queue.destroy();
-      return await interaction.reply({
-        content: "I can't seem to be able to join that voice channel!",
-        ephemeral: true,
-      });
+      return errorSend(
+        interaction,
+        "Let me in!",
+        "I'm not able to join the voice channel you're in! Make sure I have the proper permissions."
+      );
     }
 
     await interaction.deferReply();
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const { Client, Collection, Intents } = require("discord.js");
 const { token } = require("./config.json");
+const { Player } = require("discord-player");
+const { errorSend } = require("./templates/error.js");
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES],
@@ -16,6 +18,11 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
+const player = new Player(client);
+player.on("trackStart", (queue, track) =>
+  queue.metadata.channel.send(`🎶💜 | Now playing **${track.title}**!`)
+);
+
 client.once("ready", () => {
   console.log("Ready!");
 });
@@ -28,13 +35,14 @@ client.on("interactionCreate", async (interaction) => {
   if (!command) return;
 
   try {
-    await command.execute(interaction, client);
+    await command.execute(interaction, player);
   } catch (error) {
     console.error(error);
-    return interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
+    return errorSend(
+      interaction,
+      "Oops!",
+      "Looks like something went wrong while executing that command!"
+    );
   }
 });
 
